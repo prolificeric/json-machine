e6d8f391968b81ec95e2ab70847106fdf83838cb
Eric Weber <eric@prolificinteractive.com>
2019-03-12 17:31:25 -0400

    Add server

--- api.js
+++ api.js
@@ -1,3 +1,19 @@
-module.exports = (req, resp) => {
-  resp.end('test');
-};
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.get('/compute', async (req, resp) => {
+  const program = JSON.parse(req.query);
+  const result = await core.compute(program);
+  const value = await result.returnValue;
+  resp.json(value);
+});
+
+app.post('/compute', bodyParser.json(), async (req, resp) => {
+  const program = req.body;
+  const result = await core.compute(program);
+  const value = await result.returnValue;
+  resp.json(value);
+});
+
+module.exports = app;
